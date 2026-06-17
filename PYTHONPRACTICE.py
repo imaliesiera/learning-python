28f947915ab1432cecb949f472494449b252c658
Imali Esiera <imaliesiera@gmail.com>
2026-06-18 00:54:59 +0300

    Using Multiple Variables

--- PYTHONPRACTICE.py
+++ PYTHONPRACTICE.py
@@ -21,4 +21,18 @@ print(test_var)
 my_var = my_var + 3
 # Print the value assigned to my_var
 print(my_var)
+# Create variables
+num_years = 4
+days_per_year = 365
+hours_per_day = 24
+mins_per_hour = 60
+secs_per_min = 60
+# Calculate number of seconds in four years
+total_secs = secs_per_min * mins_per_hour * hours_per_day * days_per_year * num_years
+print(total_secs)
+# Update to include leap years
+days_per_year = 365.25
+# Calculate number of seconds in four years
+total_secs = secs_per_min * mins_per_hour * hours_per_day * days_per_year * num_years
+print(total_secs)
 
