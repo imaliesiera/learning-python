27c3bd30752018f7ef0f67290b0bf61e15f55efd
Imali Esiera <imaliesiera@gmail.com>
2026-06-18 00:46:48 +0300

    Manipulating Variables

--- PYTHONPRACTICE.py
+++ PYTHONPRACTICE.py
@@ -8,4 +8,17 @@ print(3*2)
 test_var = 4 + 5
 # Print the value of test_var
 print(test_var)
+# Set the value of a new variable to 3
+my_var = 3
+# Print the value assigned to my_var
+print(my_var)
+# Change the value of the variable to 100
+my_var = 100
+# Print the new value assigned to my_var
+print(my_var)
+print(test_var)
+# Increase the value by 3
+my_var = my_var + 3
+# Print the value assigned to my_var
+print(my_var)
 
