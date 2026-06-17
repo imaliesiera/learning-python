450d7cce8dd966da7d4e3dbed63290bcb519d902
Imali Esiera <imaliesiera@gmail.com>
2026-06-18 00:38:28 +0300

    Intro to Variables

--- PYTHONPRACTICE.py
+++ PYTHONPRACTICE.py
@@ -4,4 +4,8 @@ print(9-5)
 print(((1+3)*(9-2)/2)**2)
 # Multiply 3 by 2
 print(3*2)
+# Create a variable called test_var and give it a value of 4+5
+test_var = 4 + 5
+# Print the value of test_var
+print(test_var)
 
